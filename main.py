2c4d529a1eb7b5255075e4573b072e746cad2c69
Gabriel Fonseca <gabrielfonseca91@hotmail.com>
2024-11-12 08:52:57 -0300

    Remove logging setup, secret handling, and weather API request; set headless=True.

--- main.py
+++ main.py
@@ -4,24 +4,6 @@ import time
 import logging
 import logging.handlers
 import os
-import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
 
 def clean_text(text):
     return text.replace("\t", "").replace("\n\n\n\n", " ").replace("\n\n\n", "").strip()
@@ -29,7 +11,7 @@ def clean_text(text):
 async def get_agenda(playwright):
     url = "https://barbixas.com.br"
     chromium = playwright.chromium
-    browser = await chromium.launch(headless=False)
+    browser = await chromium.launch(headless=True)
     context = await browser.new_context()
     page = await context.new_page()
     await page.set_viewport_size({"width": 1920, "height": 1080})
@@ -85,11 +67,4 @@ async def main():
         await get_agenda(playwright)
 
 if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
     asyncio.run(main())
